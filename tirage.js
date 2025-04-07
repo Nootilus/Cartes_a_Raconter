59a4ef748a6b1f5c1e18496f92d0c2204bda2cef
Vincent Corlaix <vcorlaix@proton.me>
2025-04-07 17:28:58 +0200

    v0.8d - WIP refonte résultats

--- tirage.js
+++ tirage.js
@@ -113,7 +113,7 @@ function tirerCartes() {
             // Afficher le deck choisi
             const choixUnivers = document.createElement('h3');
             choixUnivers.className = "univers";
-            choixUnivers.innerHTML = '<img  class="pictos" src="svg/ico_'+ univers[1] +'.svg" alt="'+ univers[1] +'" title="'+ univers[1] +'" />&nbsp;Dans l’univers '+ univers[0] +'&nbsp;<img  class="pictos" src="svg/ico_'+ univers[1] +'.svg" alt="'+ univers[1] +'" title="'+ univers[1] +'" />';
+            choixUnivers.innerHTML = '<img class="pictos" src="svg/ico_'+ univers[1] +'.svg" alt="'+ univers[1] +'" title="'+ univers[1] +'" />&nbsp;Dans l’univers '+ univers[0] +'&nbsp;<img  class="pictos" src="svg/ico_'+ univers[1] +'.svg" alt="'+ univers[1] +'" title="'+ univers[1] +'" />';
             resultDiv.appendChild(choixUnivers);
 
             // Tirer les cartes pour chaque personne
@@ -170,7 +170,7 @@ function tirerCartes() {
                 participantDiv.innerHTML = `
                     <table>
                         <tr>
-                            <td class="celParticipant celPicto"><img class="pictos" src="svg/participant.svg" alt="Participant" title="Le participant"></td>
+                            <td class="celParticipant celPicto"><svg class="picticone"><use href="icones.svg#Participant"></use></svg></td>
                             <td class="celTab celParticipant">${prenoms[i].toLowerCase()}</td>
                             <td class="celPicto"><img class="pictos" src="svg/personnage.svg" alt="Personnage" title="Le personnage"></td>
                             <td class="celTab">${rPerso}</td> <!-- Carte Personnage -->
